89c42d0ac2eea0e84deac040c76dbe0263732bb5
Tobias Brewer <tbrewerecfc@googlemail.com>
2025-02-16 17:18:24 +0000

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+class NetObject
+{
+    int i, j;
+    int idx;
+};
+
+class Node
+{
+    int x, y; //Position
+};
+
+class DesignNetwork
+{
+    int N = 0; //Number of nodes
+    int M = 0; //Number of edges
+    int p = 0; //Number of particles
+};
+
+int main()
+{
+    cout << "Hello Network!" << endl;
+
+    return 0;
+}
