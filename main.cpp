a2de51ea5c74b487a9a21fafd1b46714f80e898b
Tobias Brewer <tbrewerecfc@googlemail.com>
2025-08-07 19:41:54 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -7,24 +7,65 @@ class NetObject
     int i, j; //Position
     int idx; //Index
     int w; //Weight
-    ///***Move object***///
+    NetObject(int a, int b, int wt, int index) {
+        //Constructor
+        i = a; //X position
+        j = b; //Y position
+        w = wt; //Weight
+        idx = index;
+    }
+    ~NetObject() {}
+    void move(int i_new, int j_new) {
+        //Move object
+        i = i_new;
+        j = j_new;
+    }
     ///***Delete object***///
     ///***Print object***///
-    ///***Check weight***///
+    int gw() {
+        //Get weight
+        return w;
+    }
+    void sw(int wt) {
+        //Set weight
+        w = wt;
+    }
+    int gx() {
+        //Get x position
+        return i;
+    }
+    int gy() {
+        //Get y position
+        return j;
+    }
+    ///***Set position***///
+    int gi() {
+        //Get index
+        return idx;
+    }
+    ///***Set index***///
 };
 
 class Node: NetObject
 {
     ///***Needs an internal edge list***///
     ///***Needs an internal particle list***///
+    ///***Constructor***///
+    ///***Destructor***///
 };
 
 class Edge: NetObject
 {
+    ///***Upper node, lower node***///
+    ///***Constructor***///
+    ///***Destructor***///
 };
 
 class Particle: NetObject
 {
+    ///***Node Occupying***///
+    ///***Constructor***///
+    ///***Destructor***///
 };
 
 class DesignNetwork
